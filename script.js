e4f915ac938dcd0b7f82c529c69df2c7a62c2afa
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-10 19:22:43 +0900

    Add the beginning of click button function

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ let operator = null;
 let operand1 = null;
 let operand2 = null;
 const displayBox = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
 function updateDisplay()
 {
     displayBox.textContent = display;
-}
+};
 
 updateDisplay();
+
+function clickButton()
+{
+    for (let i=0; i<buttons.length; i++)
+    {
+        buttons[i].addEventListener('click', function()
+        {
+            if (buttons[i].classList.contains('operand'))
+            {
+                getInput();
+                updateDisplay();
+            };
+        });
+    };
+};
+
+clickButton();
